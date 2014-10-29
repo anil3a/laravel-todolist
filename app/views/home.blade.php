fecb1f98d03c2f958b730be0ce60246505be4924
anil3a <anilprz3@gmail.com>
2014-10-29 11:55:51 +1100

    init to do task app into fortrabbit

---
+++ app/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.default')
+@section('content')
+   <h1>Your Items <a href="{{ URL::route('logout') }}" class="smallFont Ralign">Logout</a> <a href="/new" class="smallFont Ralign">New Task</a></h1>
+
+   <ul>
+   @foreach($items as $item)
+        <li>
+            {{ Form::open() }}
+                <input
+                type="checkbox"
+                class="inliner"
+                name="id"
+                value="{{ $item->id }}"
+                {{ $item->done ? "checked" : "" }}
+                onclick="this.form.submit()"
+                >
+
+                <input
+                type="hidden"
+                name="id"
+                value="{{ $item->id }}"
+                >
+
+                {{ e($item->name) }}
+
+                <a href="{{URL::route('delete', $item->id)}}">(x)</a>
+
+            {{ Form::close() }}
+        </li>
+   @endforeach
+   </ul>
+
+   <div class="paginationItems">
+    {{$items->links()}}
+   </div>
+
+
+@endsection
+
+
---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>To Do List in Laravel Framework</title>
+<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
