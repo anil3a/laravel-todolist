6898c1a38df2a9c22b073c6837a11100254477ef
anil3a <anilprz3@gmail.com>
2014-10-30 12:25:59 +1100

    change item to task and some improvements

--- todolist/app/controllers/HomeController.php
+++ todolist/app/controllers/HomeController.php
@@ -1,21 +1,26 @@
 <?php
-
+/**
+ * User: Anil
+ * Date: 20/10/2014
+ * Time: 10:50 AM
+ * Home Controller
+ */
 class HomeController extends BaseController {
 
 	public function getIndex()
 	{
-        $items = Auth::user()->items()->paginate(6);
-		return View::make('home')->with('items', $items);
+        $tasks = Auth::user()->tasks()->paginate(6);
+		return View::make('home')->with('tasks', $tasks);
 	}
 
     public function postIndex()
     {
         $id = Input::get('id');
         $userId = Auth::user()->id;
-        $item = Item::findOrFail($id);
+        $task = Task::findOrFail($id);
 
-        if($item->owner_id == $userId) {
-            $item->mark();
+        if($task->owner_id == $userId) {
+            $task->mark();
         }
         return Redirect::route('home');
     }
@@ -37,16 +42,16 @@ class HomeController extends BaseController {
             return Redirect::route('new')->withErrors($validator);
         }
 
-        $item = new Item;
-        $item->name = Input::get('name');
-        $item->owner_id = $userId;
-        $item->save();
+        $task = new Task;
+        $task->name = Input::get('name');
+        $task->owner_id = $userId;
+        $task->save();
 
         return Redirect::route('home');
 
     }
 
-    public function getDelete(Item $task)
+    public function getDelete(Task $task)
     {
         if( $task->owner_id == Auth::user()->id)
         {
